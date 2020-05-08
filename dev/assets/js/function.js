77370187f00d3b852798f1b1ea63afdc96bb7b63
skyrocket <skyrocket.kr@gmail.com>
2020-05-08 19:43:07 +0900

    1일차

--- dev/assets/js/function.js
+++ dev/assets/js/function.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function () {});
 
-})
+function showModal(el) {
+  document.querySelector(el).style.display = "flex";
+}
+
+$(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+  $(this).parents(".modal").hide();
+});
