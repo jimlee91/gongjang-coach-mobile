79295858a1d02c3acf9d104ad63221db2a6393a4
skyrocket <skyrocket.kr@gmail.com>
2020-05-10 03:11:14 +0900

    2일차 끝

--- dev/assets/js/function.js
+++ dev/assets/js/function.js
@@ -1,4 +1,24 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $('.file input[type="file"]').on("change", function (e) {
+    if ($(this).parents(".file").hasClass("muliple")) {
+      // 멀티
+    } else {
+      // 하나만
+      $(this).siblings("span").text(e.target.files[0].name);
+    }
+  });
+
+  $(".left-nav__bg").on("click", function () {
+    $("html, body").css("overflow", "auto");
+    $(".left-nav").hide();
+  });
+
+  $(".header .header__menu").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").css("overflow", "hidden");
+    $(".left-nav").show();
+  });
+});
 
 function showModal(el) {
   document.querySelector(el).style.display = "flex";
